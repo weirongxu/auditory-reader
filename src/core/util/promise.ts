d1ae103411f6f35aae405cdd107bdf2ac0cfcfd4
Weirong Xu <weirongxu.raidou@gmail.com>
2023-03-25 18:06:21 +0800

    fix: nextTick

--- src/core/util/promise.ts
+++ src/core/util/promise.ts
@@ -11,7 +11,7 @@ export const sleep = (ms: number) => {
 export const nextTick = () => {
   return new Promise<void>((resolve) => {
     setTimeout(() => {
-      resolve
+      resolve()
     })
   })
 }
