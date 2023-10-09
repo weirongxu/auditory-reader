7cfdc00ea3133684792399e504d797a89fb2c933
Weirong Xu <weirongxu.raidou@gmail.com>
2023-10-10 03:15:35 +0800

    feat: hotkey on not found

--- src/web/pages/not-found.tsx
+++ src/web/pages/not-found.tsx
@@ -1,6 +1,18 @@
 import { Alert } from '@mui/material'
+import { useHotkeys } from '../hotkey/hotkey-state.js'
+import { useEffect } from 'react'
+import { t } from 'i18next'
+import { useNavigate } from 'react-router-dom'
 
 export function NotFound({ title }: { title: string }) {
+  const nav = useNavigate()
+  const { addHotkeys } = useHotkeys()
+
+  // hotkey
+  useEffect(() => {
+    return addHotkeys([['u', t('hotkey.goBack'), () => nav('../../')]])
+  }, [addHotkeys, nav])
+
   return (
     <Alert title={`The ${title} not found`} severity="error">
       The {title} not found
