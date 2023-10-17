1969eaf1d3f2a93d0a5f4f11d784808aa769bb52
Weirong Xu <weirongxu.raidou@gmail.com>
2023-10-17 15:53:49 +0800

    refactor: utterer

---
+++ src/core/util/speech.ts
@@ -0,0 +1,232 @@
+import { rainStart, rainStop } from '../../web/pages/books/view/sound.js'
+import type { TextAlias } from '../../web/pages/books/view/types.js'
+import { ZH_PERSON_RULES } from '../consts.js'
+import { findLast, findLastIndex, orderBy, range } from './collection.js'
+
+type AliasResult = {
+  text: string
+  highlightOffsets: [index: number, accOffset: number][]
+}
+
+type HighlightEvent = {
+  charIndex: number
+  charLength: number
+}
+
+export type SpeakResult = 'cancel' | 'done'
+
+function replacePersonText(text: string): string {
+  for (const [key, value] of Object.entries(ZH_PERSON_RULES)) {
+    text = text.replaceAll(key, value.word)
+  }
+  return text
+}
+
+type QoutePostion = { type: 'start' | 'end'; charIndex: number }
+function getQoutePostions(text: string): QoutePostion[] {
+  const pos: QoutePostion[] = []
+  for (const [i, m] of [...text.matchAll(/"|'/g)].entries()) {
+    if (m.index === undefined) continue
+    if (i % 2 === 0) {
+      pos.push({
+        type: 'start',
+        charIndex: m.index,
+      })
+    } else {
+      pos.push({
+        type: 'end',
+        charIndex: m.index,
+      })
+    }
+  }
+  for (const m of text.matchAll(/“|‘|「|『/g)) {
+    if (m.index === undefined) continue
+    pos.push({
+      type: 'start',
+      charIndex: m.index,
+    })
+  }
+  for (const m of text.matchAll(/”|’|」|』/g)) {
+    if (m.index === undefined) continue
+    pos.push({
+      type: 'end',
+      charIndex: m.index,
+    })
+  }
+  return orderBy(pos, 'asc', (p) => p.charIndex)
+}
+
+export class Speech {
+  public utterance: SpeechSynthesisUtterance
+  public state: 'speaking' | 'none' | 'cancel' = 'none'
+
+  set text(text: string) {
+    this.utterance.text = text
+  }
+
+  set voice(voice: SpeechSynthesisVoice | null) {
+    this.utterance.voice = voice
+  }
+
+  set rate(rate: number) {
+    this.utterance.rate = rate
+  }
+
+  constructor() {
+    this.utterance = new SpeechSynthesisUtterance()
+  }
+
+  private aliasReplace(text: string, aliasArray: TextAlias[]): AliasResult {
+    if (!aliasArray.length)
+      return {
+        text,
+        highlightOffsets: [],
+      }
+
+    const highlightOffsets: [index: number, accOffset: number][] = []
+    const indexMap = new Map<number, TextAlias>()
+    for (const alias of aliasArray) {
+      let i = -1
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        i = text.indexOf(alias.source, i + 1)
+        if (i === -1) break
+        else if (!indexMap.has(i)) {
+          indexMap.set(i, alias)
+        }
+      }
+    }
+    const orderedIndexes = orderBy([...indexMap.entries()], 'asc', ([i]) => i)
+    let accOffset = 0
+    for (const [i, { source, target }] of orderedIndexes) {
+      text = text.replace(source, target)
+      const unitOffset = source.length - target.length
+      if (unitOffset === 0) continue
+      const afterI = i - accOffset
+      highlightOffsets.push([afterI, accOffset])
+      for (const j of range(1, target.length)) {
+        highlightOffsets.push([
+          afterI + j,
+          accOffset + Math.floor(unitOffset * (j / target.length)),
+        ])
+      }
+      accOffset = accOffset + unitOffset
+    }
+    return { text, highlightOffsets }
+  }
+
+  cancel() {
+    this.state = 'cancel'
+    if (speechSynthesis.speaking) speechSynthesis.cancel()
+  }
+
+  async speak(
+    text: string,
+    {
+      isPersonReplace,
+      quote = true,
+      alias,
+      onBoundary,
+    }: {
+      isPersonReplace: boolean
+      quote?: boolean
+      alias?: TextAlias[]
+      onBoundary?: (event: HighlightEvent) => void
+    },
+  ) {
+    this.state = 'speaking'
+    text = isPersonReplace ? replacePersonText(text) : text
+
+    // boundary
+    const boundaryListeners: ((event: SpeechSynthesisEvent) => void)[] = []
+
+    if (quote) {
+      const quotePostions = getQoutePostions(text)
+      boundaryListeners.push((event: SpeechSynthesisEvent) => {
+        // quote & rain
+        const quotePosIndex = findLastIndex(
+          quotePostions,
+          (p) => p.charIndex <= event.charIndex,
+        )
+        if (quotePosIndex === undefined) return rainStop()
+        const posPass = quotePostions.slice(0, quotePosIndex + 1)
+        const startPosList = posPass.filter((p) => p.type === 'start')
+        const endPosList = posPass.filter((p) => p.type === 'end')
+        if (startPosList.length > endPosList.length) {
+          rainStart()
+        } else {
+          rainStop()
+        }
+      })
+    }
+
+    if (alias && alias.length) {
+      const aliasResult = this.aliasReplace(text, alias)
+      text = aliasResult.text
+
+      if (onBoundary) {
+        const highlightOffsetTable = aliasResult.highlightOffsets
+
+        const highlightOffsetFn: (event: HighlightEvent) => HighlightEvent =
+          highlightOffsetTable.length
+            ? (event) => {
+                const startOffset = findLast(
+                  highlightOffsetTable,
+                  ([i]) => i <= event.charIndex,
+                )
+                if (!startOffset) return event
+                const charEndIndex = event.charIndex + event.charLength
+                const endOffset = findLast(
+                  highlightOffsetTable,
+                  ([i]) => i <= charEndIndex,
+                )
+                if (!endOffset) return event
+                const charIndex = event.charIndex + (startOffset[1] ?? 0)
+                const charLength =
+                  charEndIndex + (endOffset[1] ?? 0) - charIndex
+                return {
+                  charIndex,
+                  charLength,
+                }
+              }
+            : (event) => event
+
+        boundaryListeners.push((event: SpeechSynthesisEvent) => {
+          onBoundary(highlightOffsetFn(event))
+        })
+      }
+    }
+
+    for (const boundaryListener of boundaryListeners) {
+      this.utterance.addEventListener('boundary', boundaryListener)
+    }
+
+    this.utterance.text = text
+    speechSynthesis.speak(this.utterance)
+
+    const result = await new Promise<SpeakResult>((resolve, reject) => {
+      this.utterance.addEventListener(
+        'end',
+        () => {
+          resolve(this.state === 'cancel' ? 'cancel' : 'done')
+        },
+        { once: true },
+      )
+      this.utterance.addEventListener(
+        'error',
+        (error) => {
+          if (this.state === 'cancel') resolve('cancel')
+          else reject(error)
+        },
+        { once: true },
+      )
+    })
+
+    for (const boundaryListener of boundaryListeners) {
+      this.utterance.removeEventListener('boundary', boundaryListener)
+    }
+    rainStop()
+    this.state = 'none'
+    return result
+  }
+}
--- src/web/pages/books/view/utterer.ts
+++ src/web/pages/books/view/utterer.ts
@@ -1,10 +1,4 @@
-import { ZH_PERSON_RULES } from '../../../../core/consts.js'
-import {
-  findLast,
-  findLastIndex,
-  orderBy,
-  range,
-} from '../../../../core/util/collection.js'
+import { Speech, type SpeakResult } from '../../../../core/util/speech.js'
 import { Highlight } from './highlight.js'
 import type { Player } from './player'
 import type { PlayerIframeController } from './player-iframe-controller.js'
@@ -12,68 +6,17 @@ import type { PlayerStatesManager } from './player-states'
 import {
   nextPagePlay,
   pressEnterPlay,
-  rainStart,
-  rainStop,
   rewindPlay,
   shutterPlay,
 } from './sound.js'
-import type { ReadablePartText, TextAlias } from './types.js'
-
-function replacePersonText(text: string): string {
-  for (const [key, value] of Object.entries(ZH_PERSON_RULES)) {
-    text = text.replaceAll(key, value.word)
-  }
-  return text
-}
-
-type QoutePostion = { type: 'start' | 'end'; charIndex: number }
-function getQoutePostions(text: string): QoutePostion[] {
-  const pos: QoutePostion[] = []
-  for (const [i, m] of [...text.matchAll(/"|'/g)].entries()) {
-    if (m.index === undefined) continue
-    if (i % 2 === 0) {
-      pos.push({
-        type: 'start',
-        charIndex: m.index,
-      })
-    } else {
-      pos.push({
-        type: 'end',
-        charIndex: m.index,
-      })
-    }
-  }
-  for (const m of text.matchAll(/“|‘|「|『/g)) {
-    if (m.index === undefined) continue
-    pos.push({
-      type: 'start',
-      charIndex: m.index,
-    })
-  }
-  for (const m of text.matchAll(/”|’|」|』/g)) {
-    if (m.index === undefined) continue
-    pos.push({
-      type: 'end',
-      charIndex: m.index,
-    })
-  }
-  return orderBy(pos, 'asc', (p) => p.charIndex)
-}
-
-type AliasResult = {
-  text: string
-  highlightOffsets: [index: number, accOffset: number][]
-}
-
-type SpeakResult = 'cancel' | 'done'
+import type { ReadablePartText } from './types.js'
 
 const speakRetriedMax = 3
 
 export class Utterer {
   hl: Highlight
-  state: 'speaking' | 'none' | 'cancel' = 'none'
   states: PlayerStatesManager
-  utterance: SpeechSynthesisUtterance
+  speech: Speech
 
   constructor(
     public player: Player,
@@ -83,170 +26,35 @@ export class Utterer {
     this.states = states
     this.hl = new Highlight(iframeCtrler, states)
 
-    this.utterance = new SpeechSynthesisUtterance()
+    this.speech = new Speech()
+
     this.states.uiEvents.on('voice', (voice) => {
-      this.utterance.voice = voice
+      this.speech.voice = voice
     })
     this.states.uiEvents.on('speechSpeed', (speechSpeed) => {
-      this.utterance.rate = speechSpeed
+      this.speech.rate = speechSpeed
     })
   }
 
   cancel() {
-    this.state = 'cancel'
-    if (speechSynthesis.speaking) speechSynthesis.cancel()
-  }
-
-  private aliasReplace(text: string): AliasResult {
-    if (!this.player.iframeCtrler.alias.length)
-      return {
-        text,
-        highlightOffsets: [],
-      }
-
-    const highlightOffsets: [index: number, accOffset: number][] = []
-    const indexMap = new Map<number, TextAlias>()
-    for (const alias of this.player.iframeCtrler.alias) {
-      let i = -1
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        i = text.indexOf(alias.source, i + 1)
-        if (i === -1) break
-        else if (!indexMap.has(i)) {
-          indexMap.set(i, alias)
-        }
-      }
-    }
-    const orderedIndexes = orderBy([...indexMap.entries()], 'asc', ([i]) => i)
-    let accOffset = 0
-    for (const [i, { source, target }] of orderedIndexes) {
-      text = text.replace(source, target)
-      const unitOffset = source.length - target.length
-      if (unitOffset === 0) continue
-      const afterI = i - accOffset
-      highlightOffsets.push([afterI, accOffset])
-      for (const j of range(1, target.length)) {
-        highlightOffsets.push([
-          afterI + j,
-          accOffset + Math.floor(unitOffset * (j / target.length)),
-        ])
-      }
-      accOffset = accOffset + unitOffset
-    }
-    return { text, highlightOffsets }
+    this.speech.cancel()
   }
 
   async speakNode(node: ReadablePartText): Promise<SpeakResult> {
-    return this.speakText(node.text, {
-      onAlias: (aliasResult) => {
-        type HighlightEvent = {
-          charIndex: number
-          charLength: number
-        }
-
-        const highlightOffsetTable = aliasResult.highlightOffsets
-
-        const highlightOffsetFn: (event: HighlightEvent) => HighlightEvent =
-          highlightOffsetTable.length
-            ? (event) => {
-                const startOffset = findLast(
-                  highlightOffsetTable,
-                  ([i]) => i <= event.charIndex,
-                )
-                if (!startOffset) return event
-                const charEndIndex = event.charIndex + event.charLength
-                const endOffset = findLast(
-                  highlightOffsetTable,
-                  ([i]) => i <= charEndIndex,
-                )
-                if (!endOffset) return event
-                const charIndex = event.charIndex + (startOffset[1] ?? 0)
-                const charLength =
-                  charEndIndex + (endOffset[1] ?? 0) - charIndex
-                return {
-                  charIndex,
-                  charLength,
-                }
-              }
-            : (event) => event
-
-        const boundaryListener = (event: SpeechSynthesisEvent) => {
-          const { charIndex: highlightIndex, charLength: highlightLength } =
-            highlightOffsetFn(event)
-
-          // range highlight
-          this.hl.highlight(node, highlightIndex, highlightLength)
-        }
-
-        this.utterance.addEventListener('boundary', boundaryListener)
-
-        return () => {
-          this.utterance.removeEventListener('boundary', boundaryListener)
-        }
+    return this.speech.speak(node.text, {
+      isPersonReplace: this.states.isPersonReplace,
+      alias: this.player.iframeCtrler.alias,
+      onBoundary: (event) => {
+        this.hl.highlight(node, event.charIndex, event.charLength)
       },
     })
   }
 
-  async speakText(
-    text: string,
-    { onAlias }: { onAlias?: (aliasResult: AliasResult) => () => void } = {},
-  ): Promise<SpeakResult> {
-    this.state = 'speaking'
-    text = this.states.isPersonReplace ? replacePersonText(text) : text
-
-    // alias
-    const aliasResult = this.aliasReplace(text)
-    text = aliasResult.text
-
-    const disposeOnAlias = onAlias?.(aliasResult)
-
-    const quotePostions = getQoutePostions(text)
-
-    // boundary
-    const boundaryListener = (event: SpeechSynthesisEvent) => {
-      // quote & rain
-      const quotePosIndex = findLastIndex(
-        quotePostions,
-        (p) => p.charIndex <= event.charIndex,
-      )
-      if (quotePosIndex === undefined) return rainStop()
-      const posPass = quotePostions.slice(0, quotePosIndex + 1)
-      const startPosList = posPass.filter((p) => p.type === 'start')
-      const endPosList = posPass.filter((p) => p.type === 'end')
-      if (startPosList.length > endPosList.length) {
-        rainStart()
-      } else {
-        rainStop()
-      }
-    }
-    this.utterance.addEventListener('boundary', boundaryListener)
-
-    this.utterance.text = text
-    speechSynthesis.speak(this.utterance)
-
-    const result = await new Promise<SpeakResult>((resolve, reject) => {
-      this.utterance.addEventListener(
-        'end',
-        () => {
-          resolve(this.state === 'cancel' ? 'cancel' : 'done')
-        },
-        { once: true },
-      )
-      this.utterance.addEventListener(
-        'error',
-        (error) => {
-          if (this.state === 'cancel') resolve('cancel')
-          else reject(error)
-        },
-        { once: true },
-      )
+  async speakText(text: string): Promise<SpeakResult> {
+    return this.speech.speak(text, {
+      isPersonReplace: this.states.isPersonReplace,
+      alias: this.player.iframeCtrler.alias,
     })
-
-    disposeOnAlias?.()
-    this.utterance.removeEventListener('boundary', boundaryListener)
-    rainStop()
-    this.state = 'none'
-    return result
   }
 
   private async nextPart() {
