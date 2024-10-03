ada6134d3343447a803235a2473356b7d411513c
Weirong Xu <weirongxu.raidou@gmail.com>
2024-10-03 15:45:25 +0800

    fix: base64 converter

--- src/core/util/converter.ts
+++ src/core/util/converter.ts
@@ -1,14 +1,24 @@
+import { isBrowser } from './browser.js'
+
 export function arrayBufferToBase64(buffer: ArrayBuffer): string {
-  let binary = ''
-  const bytes = new Uint8Array(buffer)
-  for (const byte of bytes) {
-    binary += String.fromCharCode(byte)
+  if (isBrowser) {
+    let binary = ''
+    const bytes = new Uint8Array(buffer)
+    for (const byte of bytes) {
+      binary += String.fromCharCode(byte)
+    }
+    return window.btoa(binary)
+  } else {
+    return Buffer.from(buffer).toString('base64')
   }
-  return window.btoa(binary)
 }
 
 export function base64ToArrayBuffer(base64Str: string): Uint8Array {
-  return Uint8Array.from(window.atob(base64Str), (c) => c.charCodeAt(0))
+  if (isBrowser) {
+    return Uint8Array.from(window.atob(base64Str), (c) => c.charCodeAt(0))
+  } else {
+    return Uint8Array.from(Buffer.from(base64Str, 'base64'))
+  }
 }
 
 export function arrayBufferToBuffer(ab: ArrayBuffer): Buffer {
